6fc3a2d29fe68e9eadc3a4c706d89a7d2e5d67dd
sergio <sergiogargor9@gmail.com>
2025-03-02 14:56:21 +0100

    feat: repo initialization

---
+++ ejercicio2.c
@@ -0,0 +1,30 @@
+/*
+Simular una batalla entre dos gladiadores, uno será el usuario y el otro la máquina.
+
+Cada gladiador tiene un nombre (hasta 10 caracteres), un tipo de arma, una fuerza y una salud.
+
+Los ataques dependerán de la fuerza del gladiador y del tipo de arma, restarán salud a su contrincante.
+
+Deberá haber dos tipos de arma:
+	• Espada: el ataque será la fuerza del gladiador.
+	• Lanza: en este caso será el doble de la fuerza del gladiador.
+	
+El programa deberá permitir al usuario ingresar los nombres de los dos gladiadores y datos de juego desde
+la terminal. 
+
+Al ejecutar el programa se le pasarán los datos de dos gladiadores (usuario y cpu en ese orden).
+
+Deberá pasarle los siguientes parámetros (Espartaco será el usuario, Marcus la máquina):
+./gladiadores.exe Espartaco,20,espada Marcus,10,lanza
+
+Al inicio del programa, ambos jugadores tienen una salud de 100 unidades. 
+
+Durante la batalla, el usuario tendrá la oportunidad de atacar o defenderse en cada turno mediante un menú. 
+
+En el caso de la máquina atacará con una probabilidad del 50% aleatorio.
+
+La batalla se desarrollará por turnos hasta que uno de los gladiadores quede sin salud.
+
+Al finalizar la batalla, el programa deberá mostrar el resultado y mostrar los datos del ganador de la batalla
+
+*/
